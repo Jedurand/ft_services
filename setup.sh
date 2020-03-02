27842663d046b66b737b6e540e697e20fa81c0e7
Jerome DURAND <jdurand@e2r9p15.42.fr>
2020-03-02 19:29:17 +0100

    dsadsa

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 
 eval $(minikube docker-env)
+#
+# minikube start --extra-config=apiserver.ServerRunOptions.ServiceNodePortRange=1-30000
+
+cp srcs/wordpress/wordpress.sql srcs/wordpress/wordpress-tmp.sql
+sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/wordpress/wordpress-tmp.sql
 
 docker build -t my-nginx srcs/nginx
 #docker build -t my-php srcs/php
-#docker build -t my-mariadb mariadb:tag
+
+docker build -t my-mysql srcs/mysql
+docker build -t my-wordpress srcs/wordpress
 
 kubectl apply -f ./srcs/yaml/nginx.yaml
-kubectl apply -f ./srcs/yaml/php.yaml
-kubectl apply -f ./srcs/yaml/mysql.yaml
+#kubectl apply -f ./srcs/yaml/php.yaml
 kubectl apply -f ./srcs/yaml/wordpress.yaml
+kubectl apply -f ./srcs/yaml/mysql.yaml
+kubectl apply -f ./srcs/yaml/phpmyadmin.yaml
+kubectl apply -f ./srcs/yaml/ingress.yaml > /dev/null
+
+
+#inport db
+kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
+kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/wordpress-tmp.sql
