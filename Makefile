e4e28cfd37b210f92904561ef09826591426a00b
Jerome DURAND <jdurand@e2r1p8.42.fr>
2020-03-05 17:37:57 +0100

    dsada

---
+++ Makefile
@@ -0,0 +1,25 @@
+
+FLAGS	= --v=7 --cpus 4 --vm-driver=virtualbox
+
+
+all:
+	echo "<make> clean_pods, start_machine, get_ip, elete, stop"	
+
+clean_pods:
+	kubectl delete --all pods --namespace=default ;
+	kubectl delete --all deployments --namespace=default ;
+	kubectl delete --all services --namespace=default ;
+	echo "All pods deleted"
+
+start_machine:
+	minikube start $(FLAGS)
+
+get_ip:
+	echo $(minikube ip)
+
+delete:
+	minkube delete
+
+stop:
+	minikube stop
+
--- setup.sh
+++ setup.sh
@@ -8,21 +8,19 @@ export MINIKUBE_IP=$(minikube ip)
 echo $(minikube ip)
 echo $MINIKUBE_IP
 
-cp srcs/wordpress/wordpress.sql srcs/wordpress/wordpress-tmp.sql
-sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/wordpress/wordpress-tmp.sql
+#cp srcs/wordpress/wordpress.sql srcs/wordpress/wordpress-tmp.sql
+#sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/wordpress/wordpress-tmp.sql
 
 
 docker build -t my-nginx srcs/nginx
-#docker build -t my-php srcs/php
-
 docker build -t my-mysql srcs/mysql
 docker build -t my-wordpress srcs/wordpress
+docker build -t my-php srcs/php
 #docker build -t my-ftps srcs/ftp
 
 
 
 kubectl apply -f ./srcs/yaml/nginx.yaml
-
 kubectl apply -f ./srcs/yaml/mysql.yaml
 kubectl apply -f ./srcs/yaml/wordpress.yaml
 kubectl apply -f ./srcs/yaml/phpmyadmin.yaml
@@ -32,5 +30,5 @@ kubectl apply -f ./srcs/yaml/ingress.yaml > /dev/null
 
 
 #inport db
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/wordpress-tmp.sql
+#kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
+#kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/wordpress-tmp.sql
