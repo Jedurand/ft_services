005d478abc0762a7586a062abcd44a742321e405
Jerome DURAND <jdurand@e1r9p11.42.fr>
2020-03-10 20:16:46 +0100

    ftps

--- setup2.sh
+++ setup2.sh
@@ -9,7 +9,7 @@ function apply_yaml()
 	printf "✓	$@ deployed!\n"
 }
 
-SERVICE_LIST="mysql phpmyadmin nginx wordpress"
+SERVICE_LIST="mysql phpmyadmin nginx wordpress influxdb telegraf ftps"
 
 if [[ $1 = 'clean' ]]
 then
@@ -39,14 +39,14 @@ eval $(minikube docker-env)
 
 cp srcs/wordpress/files/wordpress.sql srcs/wordpress/files/wordpress-tmp.sql
 sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/wordpress/files/wordpress-tmp.sql
-#cp srcs/ftps/scripts/start.sh srcs/ftps/scripts/start-tmp.sh
-#sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/ftps/scripts/start-tmp.sh
+cp srcs/ftps/scripts/start.sh srcs/ftps/scripts/start-tmp.sh
+sed -i '' "s/\$ADDRESS/$MINIKUBE_IP/g" srcs/ftps/scripts/start-tmp.sh
 
 docker build -t mysql_alpine srcs/mysql
 docker build -t wordpress_alpine srcs/wordpress
 docker build -t nginx_alpine srcs/nginx
 docker build -t phpmyadmin srcs/phpmyadmin
-#docker build -t ftps_alpine srcs/ftps
+docker build -t ftps_alpine srcs/ftps
 #docker build -t grafana_alpine srcs/grafana
 
 for SERVICE in $SERVICE_LIST
@@ -59,5 +59,5 @@ kubectl apply -f srcs/ingress.yaml > /dev/null
 kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
 kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/files/wordpress-tmp.sql
 
-rm -rf srcs/ftps/scripts/start-tmp.sh
+#rm -rf srcs/ftps/scripts/start-tmp.sh
 rm -rf srcs/wordpress/files/wordpress-tmp.sql
