c9394275ada73d51144baecdf87f78f4bf25ba46
jukazekajou <jerome.d274@gmail.com>
2020-04-12 22:45:08 +0200

    it just works

--- setup.sh
+++ setup.sh
@@ -41,14 +41,11 @@ eval $(minikube docker-env)
 
 echo "UPDATE data_source SET url = 'http://$MINIKUBE_IP:8086'" | sqlite3 srcs/grafana/grafana.db
 echo "update user set password = '59acf18b94d7eb0694c61e60ce44c110c7a683ac6a8f09580d626f90f4a242000746579358d77dd9e570e83fa24faa88a8a6', salt = 'F3FAxVm33R' where login = 'admin'; exit" | sqlite3 srcs/grafana/grafana.db
-sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/telegraf.yaml
 
-cp srcs/wordpress/files/wordpress.sql srcs/wordpress/files/wordpress-tmp.sql
-sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/wordpress/files/wordpress-tmp.sql
-sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/ftps/scripts/start.sh
-sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/telegraf.yaml
-#cp srcs/ftps/scripts/start.sh srcs/ftps/scripts/start-tmp.sh
-#sed -i '' "s/\$ADDRESS/$MINIKUBE_IP/g" srcs/ftps/scripts/start-tmp.sh
+./srcs/sed_maison srcs/telegraf.yaml "MINIKUBE_IP" "$MINIKUBE_IP"
+./srcs/sed_maison srcs/wordpress/files/wordpress.sql "MINIKUBE_IP" "$MINIKUBE_IP"
+./srcs/sed_maison srcs/ftps/scipts/start.sh "MINIKUBE_IP" "$MINIKUBE_IP"
+
 
 docker build -t influxdb_alpine srcs/influxdb
 docker build -t mysql_alpine srcs/mysql
@@ -59,13 +56,6 @@ docker build -t ftps srcs/ftps
 docker build -t telegraf_alpine srcs/telegraf
 docker build -t grafana_alpine srcs/grafana
 
-#minikube mount $WORK_DIR/srcs/grafana/grafana/config:/grafana
-
-#sleep 1
-#sed -i '' "s/MINIKUBE_IP/$MINIKUBE_IP/g" srcs/telegraf.yaml
-#sed -i.bak 's/http:\/\/'"$MINIKUBE_IP"'/http:\/\/IP/g' srcs/telegraf.yaml
-#sleep 1
-
 for SERVICE in $SERVICE_LIST
 do
 	apply_yaml $SERVICE
@@ -74,7 +64,9 @@ done
 kubectl apply -f srcs/ingress.yaml > /dev/null
 
 kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql -u root -e 'CREATE DATABASE wordpress;'
-kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/files/wordpress-tmp.sql
+kubectl exec -i $(kubectl get pods | grep mysql | cut -d" " -f1) -- mysql wordpress -u root < srcs/wordpress/files/wordpress.sql
 
-#rm -rf srcs/ftps/scripts/start-tmp.sh
-rm -rf srcs/wordpress/files/wordpress-tmp.sql
+# RESETS
+./srcs/sed_maison srcs/telegraf.yaml "$MINIKUBE_IP" "MINIKUBE_IP"
+./srcs/sed_maison srcs/ftps/scripts/start.sh "$MINIKUBE_IP" "MINIKUBE_IP"
+./srcs/sed_maison srcs/wordpress/files/wordpress.sql "$MINIKUBE_IP" "MINIKUBE_IP"
