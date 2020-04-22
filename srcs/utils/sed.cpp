6ba343caec490ca9eb4d6a0a9c31d06cdff1075d
jukazekajou <jerome.d274@gmail.com>
2020-04-22 14:58:19 +0200

    complicated

--- srcs/utils/sed.cpp
+++ srcs/utils/sed.cpp
@@ -5,11 +5,23 @@
 #include <cstdio>
 #include <fstream>
 
+int ft_strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		i++;
+	std::cout << i << std::endl;
+	return (i);
+}
+
 int main(int ac, char **av)
 {
 	std::string b = "";
 	std::string content = "";
-	int i(0);
+	int i = 0;
+	std::string b_s(av[2]);
+	int len = b_s.size();
 
 	if (ac != 4)
 		return (0);
@@ -23,13 +35,15 @@ int main(int ac, char **av)
 	std::ofstream dest(av[1]);
 	if (!dest)
 		return (0);
+	std::cout << av[2] << std::endl << av[3] << std::endl << len <<
+	std::endl;
 	while (i < content.size())
 	{
 		i = content.find(av[2], i);
-		if (i < content.npos)
+		if (i < content.size())
 		{
-			content.replace(i, strlen(av[2]), av[3]);
-			i += strlen(av[3]);
+			content.replace(i, len, av[3]);
+//			i += ft_strlen(av[3]);
 		}
 	}
 	//std::cout << content << std::endl;
